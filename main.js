4ae55d569a38326b2e9fd3da8cd71da2f8676911
gitguti <gitmelgtrrz1@gmail.com>
2023-07-17 00:31:28 -0400

    create animation

---
+++ main.js
@@ -0,0 +1,25 @@
+import { Application } from '@splinetool/runtime';
+import SplitType from 'split-type';
+import gsap from "gsap";
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/YNtdvOeSEgRGCOQs/scene.splinecode');
+
+const headline = new SplitType('h1');
+const text = new SplitType('p', {charClass: 'char2'})
+var tl = gsap.timeline({default: {ease:'ease-in'}});
+tl.from('.char', {
+  y: -300,
+  stagger:0.05,
+  delay:1
+})
+
+.from('p', {
+opacity:0
+}, "-=1.2")
+.to('h1,p', {
+  y: 60,
+  delay:.4,
+  duration:1.3
+},"-=2")
